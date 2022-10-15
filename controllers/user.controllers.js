c10dc2b8efb7d8484cba8b778522bf77e4f378a4
b0pez <blopez4434@gmail.com>
2022-10-15 18:11:06 -0300

    controlador para cambiar contraceña terminada

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import { check, validationResult } from 'express-validator';
+import bcrypt from "bcryptjs";
 import { sendEmail } from '../helpers/email.js';
 import { generarId } from '../helpers/tokens.js';
 import User from '../models/User.js';
@@ -7,6 +8,10 @@ export const login = (req, res) => {
     res.render('auth/login', {
         title: 'Login',
     })
+}
+
+export const authentica = async (req, res) => {
+    
 }
 
 export const registro = (req, res) => {
@@ -67,7 +72,6 @@ export const create_user = async (req, res) => {
         name: user.name,
         subject: 'Confirma tu cuenta',
         email: user.email,
-        token: user.token,
         message,
     });
 
@@ -110,4 +114,121 @@ export const recovertPassword = (req, res) => {
     res.render('auth/recovertPassword', {
         title: 'Recuperar contraseña',
     })
+}
+
+export const resetPassword = async (req, res) => {
+    //Validación de campos
+    await check('email').isEmail().withMessage('Email no válido').run(req);
+
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render('auth/recovertPassword', {
+            title: 'Recuperar contraseña',
+            errors: errors.array(),
+        })
+    }
+
+    //Buscar el usuario por email
+    const { email } = req.body;
+
+    const user = await User.findOne({
+        where: { email }
+    });
+
+    if (!user) {
+        return res.render('auth/recovertPassword', {
+            title: 'Recuperar contraseña',
+            errors: [{ msg: 'El email no está registrado' }],
+        })
+    }
+
+    //Generar token
+    user.token = generarId();
+    await user.save();
+
+    //Enviar email
+    const message = `Hola ${user.name}, para recuperar tu contraseña haz click en el siguiente enlace: <a href="${process.env.URL_BACKEND}/auth/recuperar-password/${user.token}">Recuperar contraseña</a>`;
+
+    sendEmail({
+        name: user.name,
+        subject: 'Recuperar contraseña',
+        email: user.email,
+        message,
+    });
+
+
+    res.render('templates/mensaje', {
+        title: 'Recuperar contraseña',
+        message: 'Enviamos un email a tu casilla de correo. Por favor, revisa tu bandeja de entrada y spam.',
+    });
+
+
+}
+
+export const comprobarToken = async (req, res) => {
+    const { token } = req.params;
+
+    const user = await User.findOne({
+        where: { token }
+    });
+
+    if(!user){
+        return res.render('auth/confirmar-cuenta', {
+            title: 'Error al confirmar cuenta',
+            message: 'Hubo un error al confirmar tu cuenta. Por favor, intenta nuevamente.',
+            error: true,
+        })
+    }
+
+    //Mostrar formulario para cambiar contraseña
+    res.render('auth/reset-password', {
+            title: 'Cambiar contraseña',
+    })
+
+}
+
+
+export const newPassword = async (req, res) => {
+
+    //Validar password
+    await check('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres').run(req);
+    await check('password2').equals(req.body.password).withMessage('Las contraseñas no coinciden').run(req);
+
+    let errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.render('auth/reset-password', {
+            title: 'Cambiar contraseña',
+            errors: errors.array(),
+        })
+    }
+
+    const { token } = req.params;
+    const { password } = req.body;
+
+    //Buscar el usuario por token
+    const user = await User.findOne({
+        where: { token }
+    });
+
+    if(!user){
+        return res.render('auth/confirmar-cuenta', {
+            title: 'Error al confirmar cuenta',
+            message: 'Hubo un error al confirmar tu cuenta. Por favor, intenta nuevamente.',
+            error: true,
+        })
+    }
+
+    //Hashear el password
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(password, salt);
+    user.token = null;
+    await user.save();
+
+    res.render('auth/confirmar-cuenta', {
+        title: 'Password Restablecido',
+        message: 'Tu contraseña ha sido cambiada. Ya puedes iniciar sesión.',
+    })
+
 }
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { confirmAccount, create_user, login, recovertPassword, registro } from '../controllers/user.controllers.js';
+import { authentica, comprobarToken, confirmAccount, create_user, login, newPassword, recovertPassword, registro, resetPassword } from '../controllers/user.controllers.js';
 
 const router = Router();
 
 router.get('/login', login);
+router.post('/login', authentica);
 
 router.get('/registro', registro);
 router.post('/registro', create_user);
@@ -12,9 +13,8 @@ router.post('/registro', create_user);
 router.get('/confirm/:token', confirmAccount);
 
 router.get('/recuperar-password', recovertPassword);
-
-router.post('/', (req, res) => {
-
-});
+router.post('/recuperar-password', resetPassword);
+router.get('/recuperar-password/:token', comprobarToken);
+router.post('/recuperar-password/:token', newPassword);
 
 export default router;
