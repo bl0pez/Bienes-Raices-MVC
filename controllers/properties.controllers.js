e1fd84e14529ed38dda0d33f9039ca139fb8ee7c
b0pez <blopez4434@gmail.com>
2022-10-16 09:18:43 -0300

    vistas crear propiedad add

--- controllers/properties.controllers.js
+++ controllers/properties.controllers.js
@@ -1,3 +1,18 @@
 export const admin = (req, res) => {
-    
+    res.render('properties/admin', {
+        title: 'Mis Propiedades',
+        isLogin: true,
+    });
+}
+
+//Vista de Crear Propiedad
+export const getCreate = (req, res) => {
+    res.render('properties/crear', {
+        title: 'Crear Propiedad',
+        isLogin: true,
+    });
+}
+
+export const postCreate = (req, res) => {
+    res.send('Post Create');
 }
--- routes/properties.router.js
+++ routes/properties.router.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { admin } from '../controllers/properties.controllers.js';
+import { admin, getCreate } from '../controllers/properties.controllers.js';
 
 const router = Router();
 
 router.get('/mis-propiedades', admin);
+
+router.get('/propiedades/crear', getCreate);
+router.post('/propiedades/crear', )
 
 export default router;
