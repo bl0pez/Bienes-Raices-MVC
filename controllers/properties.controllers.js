598ba05f1161fd483876c1fc201074c3ee7582e4
b0pez <blopez4434@gmail.com>
2023-02-04 09:54:07 -0300

    utimos cambios

--- controllers/properties.controllers.js
+++ controllers/properties.controllers.js
@@ -1,10 +1,27 @@
+import { unlink } from 'node:fs';
 import { validationResult } from "express-validator";
-import upload from "../middleware/subirImagen.js";
 import { Propertie, Price, Category } from '../models/index.js'
 
-export const admin = (req, res) => {
+export const admin = async (req, res) => {
+
+    const { id } = req.user;
+
+    const properties = await Propertie.findAll({
+        where: {
+            userId: id
+        },
+        include: [{
+            model: Category, as: 'category',
+        },
+        {
+            model: Price, as: 'price',
+        }]
+    });
+
+
     res.render('properties/admin', {
         title: 'Mis Propiedades',
+        properties
     });
 }
 
@@ -38,6 +55,8 @@ export const postCreate = async (req, res) => {
             datos: req.body
         });
 
+        return;
+
     }
 
     const { title, description, address, category: categoryId, price, rooms, wc, parking, lat, lng } = req.body;
@@ -57,7 +76,7 @@ export const postCreate = async (req, res) => {
             categoryId,
             priceId: price,
             userId: req.user.id,
-            image: 'aaaaapng'
+            image: "noImage.jpg",
         });
 
         const { id } = propertie;
@@ -80,17 +99,17 @@ export const addImage = async (req, res) => {
     const propertie = await Propertie.findByPk(id);
 
     if (!propertie) {
-        res.redirect('/mis-propiedades');
+        return res.redirect('/mis-propiedades');
     }
 
     //Validar que la propiedad no este publicada
     if (propertie.published) {
-        res.redirect('/mis-propiedades');
+        return res.redirect('/mis-propiedades');
     }
 
     //Validar que la propiedad pertenezca al usuario
     if (propertie.userId !== req.user.id) {
-        res.redirect('/mis-propiedades');
+        return res.redirect('/mis-propiedades');
     }
 
 
@@ -105,22 +124,21 @@ export const uploadImage = async (req, res, next) => {
 
     const { id } = req.params;
 
-    console.log(req.file);
     //Validar que la propiedad exista
     const propertie = await Propertie.findByPk(id);
 
     if (!propertie) {
-        res.redirect('/mis-propiedades');
+        return res.redirect('/mis-propiedades');
     }
 
     //Validar que la propiedad no este publicada
     if (propertie.published) {
-        res.redirect('/mis-propiedades');
+        return res.redirect('/mis-propiedades');
     }
 
     //Validar que la propiedad pertenezca al usuario
     if (propertie.userId !== req.user.id) {
-        res.redirect('/mis-propiedades');
+        return res.redirect('/mis-propiedades');
     }
 
 
@@ -142,4 +160,136 @@ export const uploadImage = async (req, res, next) => {
         console.log(error.message);
     }
 
+}
+
+export const editPropertie = async (req, res) => {
+    
+        const { id } = req.params;
+
+        const propertie = await Propertie.findByPk(id);
+
+        if (!propertie) {
+            return res.redirect('/mis-propiedades');
+        }
+
+        //Validar que sea el usuario que creo la propiedad
+        if (propertie.userId.toString() !== req.user.id.toString()) {
+            return res.redirect('/mis-propiedades');
+        }
+
+        const [prices, categories] = await Promise.all([Price.findAll(), Category.findAll()]);
+
+
+        res.render('properties/editar', {
+            title: 'Actualizar Propiedad',
+            prices,
+            categories,
+            datos: propertie
+        });
+
+        return;
+
+
+}
+
+export const updatePropertie = async (req, res) => {
+
+
+    let resultdo = validationResult(req);
+
+    if (!resultdo.isEmpty()) {
+        const [prices, categories] = await Promise.all([Price.findAll(), Category.findAll()])
+
+        res.render('properties/editar', {
+            title: 'Editar Propiedad',
+            prices,
+            categories,
+            errors: resultdo.array(),
+            datos: req.body
+        });
+
+        return;
+
+    }
+
+    const { id } = req.params;
+
+    const propertie = await Propertie.findByPk(id);
+
+    if (!propertie) {
+        return res.redirect('/mis-propiedades');
+    }
+
+    //Validar que sea el usuario que creo la propiedad
+    if (propertie.userId.toString() !== req.user.id.toString()) {
+        return res.redirect('/mis-propiedades');
+    }
+
+    const { title, description, address, category: categoryId, price, rooms, wc, parking, lat, lng } = req.body;
+
+    try {
+        await propertie.set({
+            title,
+            description,
+            rooms,
+            parking,
+            wc,
+            address,
+            lat,
+            lng,
+            categoryId,
+            priceId: price,
+        });
+
+        await propertie.save();
+
+        res.redirect('/mis-propiedades');
+
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+
+
+export const deletePropertie = async (req, res) => {
+    const { id } = req.params;
+
+    const propertie = await Propertie.findByPk(id);
+
+    if (!propertie) {
+        return res.redirect('/mis-propiedades');
+    }
+
+    //Validar que la propiedad sea del usuario
+    if (propertie.userId.toString() !== req.user.id.toString()) {
+        return res.redirect('/mis-propiedades');
+    }
+
+
+    //Eliminar la propiedad
+    try {
+        await propertie.destroy();
+        res.redirect('/mis-propiedades');
+
+        //Eliminar la imagen
+        if(propertie.image !== "noImage.jpg"){
+            unlink(`public/uploads/${propertie.image}`, (err) => {
+                if (err) throw err;
+                console.log('Imagen Eliminada');
+            });
+
+            return;
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }   
+
+}
+
+export const viewPropertie = async (req, res) => {
+    
 }
--- routes/properties.router.js
+++ routes/properties.router.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
-import { addImage, admin, getCreate, postCreate, uploadImage } from '../controllers/properties.controllers.js';
+import { addImage, admin, deletePropertie, editPropertie, getCreate, postCreate, updatePropertie, uploadImage, viewPropertie } from '../controllers/properties.controllers.js';
 import privateRouter from '../middleware/privateRouter.js';
 import upload from '../middleware/subirImagen.js';
 import { validateBody } from '../validations/bodyValidations.js';
 
 const router = Router();
 
+//Rutas publicas
+router.get('/propiedades/:id', viewPropertie);
+
+//Rutas privadas
 router.use(privateRouter);
 
 router.get('/mis-propiedades', admin);
@@ -15,5 +19,11 @@ router.post('/propiedades/crear', validateBody,postCreate );
 
 router.get('/propiedades/agregar-image/:id', addImage);
 router.post('/propiedades/agregar-image/:id', upload.single('image'), uploadImage);
+
+router.get('/propiedades/editar/:id', editPropertie);
+router.post('/propiedades/editar/:id', validateBody, updatePropertie);
+
+router.post('/propiedades/eliminar/:id', deletePropertie);
+
 
 export default router;
